08bb08fe2ab2c5a12853d56c0e86cf6b16fb370c
Bryce Edmonds <bryce@bryceedmonds.com>
2019-05-13 14:35:41 -0600

    zoo initial push

--- app/main.js
+++ app/main.js
@@ -1,3 +1,48 @@
+class Alligator {
+  constructor(
+    name = '',
+    color = '',
+    gender = '',
+    weight = 0,
+    tailLengthInch = 0,
+    teethSize = '',
+  ) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.tailLengthInch = tailLengthInch;
+    this.teethSize = teethSize;
+  }
+  eat(food) {
+    console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+    alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+export default Alligator;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import Alligator from "./models/Alligator.js";
 
 let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
